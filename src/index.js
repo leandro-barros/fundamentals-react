b7cacd5e2b01e53989c80b851ebccb3106fffaaa
Leandro Barros <leandroedbarros@gmail.com>
2021-11-23 20:55:12 -0300

    Creating first component

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import ReactDOM from "react-dom";
 import React from 'react'
 
+import './index.css'
+
+import FirstComponent from './components/basics/FirstComponent.js'
+
 const tag = <strong>Hello React!!!</strong>;
 
 const element = document.getElementById('root');
 ReactDOM.render(
     <div>
-        { tag }
+        <FirstComponent></FirstComponent>
     </div>,
      element
 );
