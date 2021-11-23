019ff71cc2678bcab451e244eb900c2fda927a1b
Leandro Barros <leandroedbarros@gmail.com>
2021-11-23 20:26:26 -0300

    Inserting element HTML in component

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import ReactDOM from "react-dom";
+import React from 'react'
+
+const tag = <strong>Hello React!!!</strong>;
 
 const element = document.getElementById('root');
-ReactDOM.render('Hello React!!!', element);
+ReactDOM.render(
+    <div>
+        { tag }
+    </div>,
+     element
+);
