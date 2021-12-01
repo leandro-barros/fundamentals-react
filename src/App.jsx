87bd95a41b4262269efbd223030626ab16b5c48d
Leandro Barros <leandroedbarros@gmail.com>
2021-12-01 20:12:06 -0300

    Implementinh flex box na application

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 
+import './App.css'
+
 import ComParam from "./components/basics/ComParam";
 import FirstComponent from "./components/basics/FirstComponent";
 import Fragment from "./components/basics/Fragment";
+
 import Radom from "./components/Radom";
 
 import Card from './components/layout/Card'
@@ -10,30 +13,32 @@ import Card from './components/layout/Card'
 
 export default () => {
     return (
-        <div id="app">
+        <div className="app">
             <h1>Fundamentos React</h1>
 
-            <Card title="#04 - Desafio Aleatório">
-                <Radom 
-                    min={ 5 }
-                    max={ 10 } />
-            </Card>
+            <div className="cards">
+                <Card title="#04 - Desafio Aleatório">
+                    <Radom 
+                        min={ 5 }
+                        max={ 10 } />
+                </Card>
 
-            <Card title="#03 - Fragment">
-                <Fragment />
-            </Card>
+                <Card title="#03 - Fragment">
+                    <Fragment />
+                </Card>
 
-            <Card title="#02 - Com Parâmetro">
-                <ComParam 
-                    title="Situação Aluno"
-                    aluno="Leandro Barros"
-                    nota={ 9.8 } />
-            </Card>
+                <Card title="#02 - Com Parâmetro">
+                    <ComParam 
+                        title="Situação Aluno"
+                        aluno="Leandro Barros"
+                        nota={ 9.8 } />
+                </Card>
 
-            <Card title="#01 - Primeiro Componente">
-                <FirstComponent />
-            </Card>     
-                    
+                <Card title="#01 - Primeiro Componente">
+                    <FirstComponent />
+                </Card>     
+            </div>
+    
         </div>
 
     );
